d57f2608ae8c6b5de49ff3dc3c0697362d620452
Kavan Koh <kavanohk@gmail.com>
2018-09-29 23:22:48 +0800

    add notification

--- Application/src/main/java/com/example/android/bluetoothlegatt/HomeActivity.java
+++ Application/src/main/java/com/example/android/bluetoothlegatt/HomeActivity.java
@@ -1,16 +1,65 @@
 package com.example.android.bluetoothlegatt;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.view.View;
 import android.view.Window;
 
 public class HomeActivity extends Activity {
 
+//    @TargetApi(Build.VERSION_CODES.O)
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence name = "channel_name";
+            String description = "channel descript";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            String CHANNEL_ID = "1";
+            int notificationId = 1;
+            String notificationTitle = "Quest";
+            String notificationText = "Your daily quest today is available. Don't break 17 days streak!";
+
+
+            NotificationChannel channel = null;
+            channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+            // Create an explicit intent for an Activity in your app
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.icon_512)
+                    .setContentTitle(notificationTitle)
+                    .setContentText(notificationText)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    // Set the intent that will fire when the user taps the notification
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+
+            // notificationId is a unique int for each notification that you must define
+            notificationManagerCompat.notify(notificationId, mBuilder.build());
+        }
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home);
     }
